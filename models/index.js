1ff362d2e98ca6a26d01ceb9d25c122e1fcf7468
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-08-29 00:16:12 +0530

    //cloud

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-//const uri = "mongodb://localhost:27017"; 
-const uri = "mongodb+srv://jprasad_mongodbuser:tKFVQUkWBIOyL9Af@cluster0.m0iwuxt.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb://localhost:27017"; 
+//const uri = "mongodb+srv://jprasad_mongodbuser:tKFVQUkWBIOyL9Af@cluster0.m0iwuxt.mongodb.net/?retryWrites=true&w=majority"; //cloud
 
 
 function main() {
